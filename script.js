34da45a699678f9855364213a5af65b0b33d4b78
Asma Hassan <asmasheikhhassan@gmail.com>
2023-03-31 18:03:42 +0100

    Added time and styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var timeDisplayEl = $('#currentDay');
 
 function displayTime() {
-    var rightNow = moment().format('dddd, DD MMM YYYY');
+    var rightNow = moment().format('dddd, DD MMM YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
 }
 
